ea27461705542287f9cf76756c6443c524c3e610
Nirav Pansuriya <niravpansuriya@Niravs-MacBook-Air.local>
2023-04-28 10:06:03 -0400

    syntex error removed from queries.sql

--- queries.sql
+++ queries.sql
@@ -1,31 +1,31 @@
 CREATE SCHEMA IF NOT EXISTS events;
 
-CREATE TABLE events.types (
+CREATE TABLE IF NOT EXISTS events.types (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE events.play_patterns (
+CREATE TABLE IF NOT EXISTS events.play_patterns (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE events.teams (
+CREATE TABLE IF NOT EXISTS events.teams (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE events.positions (
+CREATE TABLE IF NOT EXISTS events.positions (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE events.players (
+CREATE TABLE IF NOT EXISTS events.players (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
-CREATE TABLE events.events (
+CREATE TABLE IF NOT EXISTS events.events (
     id UUID PRIMARY KEY,
     index INTEGER NOT NULL,
     match_id TEXT NOT NULL,
@@ -53,14 +53,14 @@ CREATE TABLE events.events (
     FOREIGN KEY (team_id) REFERENCES events.teams(id)
 );
 
-CREATE TABLE events.related_events (
+CREATE TABLE IF NOT EXISTS events.related_events (
     event_id UUID NOT NULL,
     related_event_id UUID NOT NULL,
     FOREIGN KEY (event_id) REFERENCES events.events(id),
     FOREIGN KEY (related_event_id) REFERENCES events.events(id)
 );
 
-CREATE TABLE events.lineups (
+CREATE TABLE IF NOT EXISTS events.lineups (
     event_id UUID NOT NULL,
     player_id INTEGER NOT NULL,
     position_id INTEGER NOT NULL,
@@ -71,11 +71,14 @@ CREATE TABLE events.lineups (
 );
 
     
-CREATE INDEX idx_index ON events.events (index);
-CREATE INDEX idx_timestamp ON events.events (timestamp);
-CREATE INDEX idx_match_id ON events.events (match_id);
-CREATE INDEX idx_location_x ON events.events (location_x);
-CREATE INDEX idx_location_y ON events.events (location_y);
+CREATE INDEX IF NOT EXISTS idx_index ON events.events (index);
+CREATE INDEX IF NOT EXISTS idx_timestamp ON events.events (timestamp);
+CREATE INDEX IF NOT EXISTS idx_match_id ON events.events (match_id);
+CREATE INDEX IF NOT EXISTS idx_location_x ON events.events (location_x);
+CREATE INDEX IF NOT EXISTS idx_location_y ON events.events (location_y);
 
 ALTER TABLE IF EXISTS events.related_events
     ADD CONSTRAINT event_id_related_event_id UNIQUE (event_id, related_event_id);
+
+ALTER TABLE IF EXISTS events.lineups
+    ADD CONSTRAINT event_id_player_id UNIQUE (event_id, player_id);
